08d68cffe9764a09697365d59f3bdde62108e2e3
Jason McAffee <jasonlmcaffee@gmail.com>
2017-11-24 14:37:29 -0700

    hit resolution

--- src/components/Bullet.js
+++ src/components/Bullet.js
@@ -8,23 +8,24 @@ const style ={
   material:{
     blueMaterial: new LineBasicMaterial({color:0x4286f4}),
     purpleMaterial: new LineBasicMaterial({color:0x7b42af}),
-    sphereMaterial: new MeshBasicMaterial({color:0x4286f4}),
+    sphereMaterial: new MeshBasicMaterial({color:0x4286f4, transparent:true, opacity:0.5}),
   },
   geometry:{
-    sphere: new SphereGeometry(1 , 32, 32)
+    sphere: new SphereGeometry(.5 , 16, 16)
   }
 };
 
 export default class Bullet{
   componentId = generateUniqueId({name:'Bullet'})
-  distancePerSecond
+  distancePerSecond //NOTE: if you go to fast, hit test is incorrect. may need other approach.
   totalDistanceTraveled = 0
   distance = 0
-  constructor({direction, distance=1000, distancePerSecond=1 , startPosition, sphereGeometry=style.geometry.sphere, sphereMaterial=style.material.sphereMaterial}={}){
+  damage
+  constructor({direction, distance=1000, distancePerSecond=300 , startPosition, damage=1,sphereGeometry=style.geometry.sphere, sphereMaterial=style.material.sphereMaterial}={}){
     this.distancePerSecond = distancePerSecond;
     this.direction = direction;
     this.distance = distance;
-
+    this.damage = damage;
     let {x, y, z} = startPosition;
     this.sphere = new Mesh(sphereGeometry, sphereMaterial);
     this.sphere.name = generateUniqueId({name:'sphere'});
@@ -40,35 +41,46 @@ export default class Bullet{
   }
 
   render({delta=this.clock.getDelta(), hittableComponents}={}) {
-    let distance = this.distancePerSecond * delta;
-    let newPosition = new Vector3().copy(this.direction).normalize().multiplyScalar(distance);
-    this.sphere.position.add(newPosition);
-    this.hitBox = new Box3().setFromObject(this.sphere);
+    if(this.stopTravelling){return;}
+    this.performHitTestForEachPositionInTime({hittableComponents, delta});
 
-    this.totalDistanceTraveled += distance;
     if(this.totalDistanceTraveled >= this.distance){
       signal.trigger(ec.stage.destroyComponent, {componentId:this.componentId});
       return;
     }
 
-    this.performHitTest({hittableComponents});
+
   }
 
-  performHitTest({hittableComponents}){
+  performHitTestForEachPositionInTime({hittableComponents, delta, hitResolution=10, direction=this.direction, distancePerSecond=this.distancePerSecond, sphere=this.sphere}){
+    for (let hr = 1; hr <= hitResolution; ++hr){
+      let distance = (distancePerSecond * delta) / hitResolution;
+      this.totalDistanceTraveled += distance;
+      let newPosition = new Vector3().copy(direction).normalize().multiplyScalar(distance);
+      sphere.position.add(newPosition);
+      this.hitBox =  new Box3().setFromObject(this.sphere);
+      let hit = this.performHitTest({hittableComponents});
+      if(hit){
+        return;
+      }
+    }
+  }
+  performHitTest({hittableComponents, hitBox=this.hitBox}){
     // console.log(`bullet performing hit test against ${hittableComponents.length} components`);
     let length = hittableComponents.length - 1;
     while(length >= 0){
       let hittableComponent = hittableComponents[length--];
       let otherHitBox = hittableComponent.hitBox;
       if(!otherHitBox){continue;}
-      if(this.hitBox.intersectsBox(otherHitBox)){
+      if(hitBox.intersectsBox(otherHitBox)){
         console.log('BULLET HIT SOMETHING ' + hittableComponent.componentId);
         signal.trigger(ec.hitTest.hitComponent, {hitComponent:hittableComponent, hitByComponent:this});
         signal.trigger(ec.stage.destroyComponent, {componentId:this.componentId});
-        return
+        this.stopTravelling = true;
+        return true;
       }
-
     }
+    return false;
   }
 
   createLine({x=0, y=0, z=0, x2=0, y2=0, z2=0, material=style.material.blueMaterial}={}){
