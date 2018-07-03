96dea8bbfebeb40eff149e01cc2443c2a908502c
Jason Mcaffee <jason.mcaffee@insidesales.com>
2018-07-03 07:09:25 -0600

    enemy moves to first path vector

--- src/components/enemies/Enemy.js
+++ src/components/enemies/Enemy.js
@@ -9,17 +9,18 @@ export default class Enemy{
   threejsObject
   hitBox
   moveClock = new Clock()
-  constructor({x=0, y=0, z=0, hitPoints=10, speed=1, fireIntervalMs=1000, firingRange=10, pathVectors=[], towerPositions=[]}={}){
+  constructor({x=0, y=0, z=0, hitPoints=10, moveDistancePerSecond=9, fireIntervalMs=1000, firingRange=10, pathVectors=[], towerPositions=[]}={}){
     const {threejsObject, hitBox} = createThreejsObjectAndHitbox({x, y, z, componentId: this.componentId});
     this.threejsObject = threejsObject;
     this.hitBox = hitBox;
     this.hitPoints = hitPoints;
-    this.speed = speed;
+    this.moveDistancePerSecond = moveDistancePerSecond;
     this.firingRange = firingRange;
     this.fireIntervalMs = fireIntervalMs;
     this.position = {x, y, z};
     this.pathVectors = pathVectors;
     this.towerPositions = towerPositions;
+    this.currentPathVectorsIndex = 0;
     signal.registerSignals(this);
   }
 
@@ -52,26 +53,34 @@ export default class Enemy{
     console.log(`Enemy firing bullet in direction: `, direction);
   }
 
-  travelPath({nearestTargetVector=this.nearestTargetVector, delta=this.moveClock.getDelta(), moveDistancePerSecond=this.moveDistancePerSecond}={}){
+  getVectorToTravelTo({pathVectors=this.pathVectors, currentPathVectorsIndex=this.currentPathVectorsIndex}={}){
+    const pathVector = pathVectors[currentPathVectorsIndex];
+    const {x2: x, y2: y, z2: z} = pathVector; //we want the second
+    return {x, y, z};
+  }
+
+  travelPath({nearestTargetVector=this.getVectorToTravelTo(), delta=this.moveClock.getDelta(), moveDistancePerSecond=this.moveDistancePerSecond}={}){
     if(!nearestTargetVector){ return;}
-    if(this.stopMovingIfYouHitEarth()){return;}
 
-    let playerPositionVector = new Vector3(nearestTargetVector.x, nearestTargetVector.y, nearestTargetVector.z);
-
+    //where we are traveling from
     let startPosition = this.threejsObject.position;
+    //where we are traveling to
+    let pathPointVector = new Vector3(nearestTargetVector.x, nearestTargetVector.y, nearestTargetVector.z);
+
+    //direction we're going
     let direction = new Vector3();
-    direction.subVectors(nearestTargetVector, startPosition);
+    direction.subVectors(pathPointVector, startPosition);
     this.currentDirection = direction;//needed so when we run into something we can back up.
 
+    //how far we've traveled
     let distance = (moveDistancePerSecond * delta);
 
+    //update position
     let newPosition = new Vector3().copy(direction).normalize().multiplyScalar(distance);
     this.threejsObject.position.add(newPosition);
     this.hitBox = new Box3().setFromObject(this.threejsObject);
-
     signal.trigger(ec.hitTest.updateComponentHitBox, {component:this});
 
-    this.performHitTest();
   }
 
   //called on when ec.stage.addComponent is triggered with this as the component. (typically done by Level)
@@ -84,6 +93,8 @@ export default class Enemy{
     let object3d = scene.getObjectByName(name);
     scene.remove(object3d);
     signal.trigger(ec.hitTest.unregisterHittableComponent, {componentId});
+    signal.unregisterSignals(this);
+    clearInterval(this.fireInterval);
   }
 }
 
